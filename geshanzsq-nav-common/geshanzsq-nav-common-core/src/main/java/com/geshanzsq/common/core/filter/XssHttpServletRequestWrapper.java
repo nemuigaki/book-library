2128c3b151875599b905fcc7620d30b3a26f3b49
nemuigaki <2094176918@qq.com>
2024-01-02 13:46:09 +0800

    fix: 使用StandardCharsets消除魔法值

--- geshanzsq-nav-common/geshanzsq-nav-common-core/src/main/java/com/geshanzsq/common/core/filter/XssHttpServletRequestWrapper.java
+++ geshanzsq-nav-common/geshanzsq-nav-common-core/src/main/java/com/geshanzsq/common/core/filter/XssHttpServletRequestWrapper.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.commons.io.IOUtils;
 
 
@@ -50,14 +52,14 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         }
 
         // 为空，直接返回
-        String json = IOUtils.toString(super.getInputStream(), "utf-8");
+        String json = IOUtils.toString(super.getInputStream(), StandardCharsets.UTF_8);
         if (StrUtils.isEmpty(json)) {
             return super.getInputStream();
         }
 
         // xss过滤
         json = EscapeUtil.clean(json).trim();
-        byte[] jsonBytes = json.getBytes("utf-8");
+        byte[] jsonBytes = json.getBytes(StandardCharsets.UTF_8);
         final ByteArrayInputStream bis = new ByteArrayInputStream(jsonBytes);
         return new ServletInputStream() {
             @Override
